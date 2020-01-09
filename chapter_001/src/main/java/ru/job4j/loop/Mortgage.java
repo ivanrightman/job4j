fbd9fa693a2e5c1abd6ca3ea0d27b789d0dc94db
ivanrightman <vivasvse@gmail.com>
2020-01-09 20:49:50 +0500

    5.7. Ипотека.[#212990]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double ostatok = amount + (amount*percent);
+        while(ostatok > 0) {
+            ostatok -= salary;
+            year++;
+        }
+        return year;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class MortgageTest {
+
+    @Test
+    public void when1Year() {
+        Mortgage check = new Mortgage();
+        int result = check.year(1000,1200, 0.10);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when2Year() {
+        Mortgage check = new Mortgage();
+        int result = check.year(100,120, 0.50);
+        assertThat(result, is(2));
+    }
+}
