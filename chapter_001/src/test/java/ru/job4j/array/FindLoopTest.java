ca448fb6af27bb7c5edac6266968f10c5e5846a1
ivanrightman <vivasvse@gmail.com>
2020-01-18 21:30:37 +0500

    6.5. Поиск индекса в диапазоне.[#212939]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.array;
 
-import com.sun.org.apache.xpath.internal.FoundIndex;
-import com.sun.prism.shader.FillRoundRect_Color_Loader;
+
 import org.junit.Test;
+
+import java.awt.peer.TextAreaPeer;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -24,6 +26,30 @@ public class FindLoopTest {
         int[] data = new int[] {20, 33, 44};
         int el = 15;
         int result = find.indexOf(data, el);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind3() {
+        FindLoop find = new FindLoop();
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = find.indexOf(data, el, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindNothing() {
+        FindLoop find = new FindLoop();
+        int[] data = new int[] {321, 123, 125, 1, 10};
+        int el = 4;
+        int start = 0;
+        int finish = 4;
+        int result = find.indexOf(data, el, start, finish);
         int expect = -1;
         assertThat(result, is(expect));
     }
