4888d0717762cae71349d61d59c1d675d199f9f4
ivanrightman <vivasvse@gmail.com>
2020-01-19 13:09:13 +0500

    6.4.1. Слово заканчивается с ...[#212938] - испр

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -5,7 +5,14 @@ public class EndsWith {
         boolean result = false;
         // проверить, что массив word имеет последние элементы одинаковые с post
         for (int index = 0; index < post.length; index++) {
-            result = word[index+word.length-post.length] == post[index];
+            //result = word[index+word.length-post.length] == post[index];
+            if (word[index+word.length-post.length] != post[index]) {
+                result = false;
+
+            }
+            else {
+                result = true;
+            }
         }
         return result;
     }
