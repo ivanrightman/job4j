c724be267b7feed8e512d7a7da514d98c0b46271
ivanrightman <vivasvse@gmail.com>
2020-01-19 19:14:05 +0500

    6.4. Слово начинается с ...[#212934] - испр_2

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = false;
-        for (int index = 0; index < (word.length - pref.length - 1); index++) {
+        for (int index = 0; index < pref.length; index++) {
             //result = word[index] == pref[index];
             if (word[index] != pref[index]) {
                 result = false;
