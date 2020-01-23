09083efce400db86f78586fab01d6e6a5c9a699e
ivanrightman <vivasvse@gmail.com>
2020-01-23 22:34:43 +0500

    6.5.3. Сортировка выборкой.[#212940]

---
+++ chapter_001/src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class SortSelectedTest {
+
+    @Test
+    public void whenSort5() {
+        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort3() {
+        int[] data = new int[] {9,6,2};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {2,6,9};
+        assertThat(result, is(expected));
+    }
+
+}
