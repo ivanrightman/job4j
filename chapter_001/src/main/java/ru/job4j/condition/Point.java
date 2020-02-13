79ed0bf7de6df945fa42c8d1b6f97f1595723b58
ivanrightman <vivasvse@gmail.com>
2020-02-14 00:19:26 +0500

    2.1. Рефакторинг - Расстояние между точками.[#213071]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,22 +1,25 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        int x = x2 - x1;
-        int y = y2 - y1;
-        double first = Math.pow(x, 2);
-        double second = Math.pow(y, 2);
-        double result = first + second;
-        return Math.sqrt(result);
+
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return Math.sqrt(pow(this.x - that.x, 2) + (pow(this.y - that.y, 2)));
     }
 
     public static void main(String[] args) {
-        int x1 = 2;
-        int x2 = 3;
-        int y1 = 4;
-        int y2 = 5;
-        double result = distance(x1, y1, x2, y2);
-        System.out.println("result (" + x1 + ", " + y1 + ")"
-                + " to (" + x2 + ", " + y2 + ") " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -7,12 +7,13 @@ public class PointTest {
 
     @Test
     public void distance() {
-        int x1 = 2;
-        int x2 = 3;
-        int y1 = 4;
-        int y2 = 5;
+        Point a = new Point(2, 4);
+        Point b = new Point(3, 5);
         double expected = 1.414;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
+
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
