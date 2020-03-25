6eb2045eb113c9d69ad02fde348d84731a933f9a
ivanrightman <vivasvse@gmail.com>
2020-03-25 18:34:46 +0500

    0.5. Иерархия исключений и множественный catch. [#219368] - испр

--- chapter_002/src/main/java/ru/job4j/ex/UserStore.java
+++ chapter_002/src/main/java/ru/job4j/ex/UserStore.java
@@ -2,49 +2,41 @@ package ru.job4j.ex;
 
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        if (login != users[0].getUsername()) {
-            throw new UserNotFoundException("user with this login not found");
-        } else {
-            users[0].setUsername("pe");
+        User user = null;
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getUsername().equals(login)) {
+                user = users[i];
+            } else {
+                throw new UserNotFoundException("user with this login not found");
+            }
         }
-        return users[0];
+        return user;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
+        boolean result;
         if (user.getUsername().length() < 3 || !user.isValid()) {
             throw new UserInvalidException("your user is invalid");
+        } else {
+            result = true;
         }
-        return false;
+        return result;
     }
 
     public static void main(String[] args) {
-        User[] users = {
-                new User("petr", true)
-        };
-
-        User[] users2 = {
-                new User("чтобы use is invalid", false)
-        };
-
         try {
-            User user = findUser(users, "чтобы login not found");
+        User[] users = {
+                new User("Pe", true)
+        };
+        User user = findUser(users, "Pe");
+
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
-
-        } catch (UserNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            User user2 = null;
-            try {
-                user2 = findUser(users2, "чтобы use is invalid");
-            } catch (UserNotFoundException e) {
-                e.printStackTrace();
-            }
-            validate(user2);
         } catch (UserInvalidException en) {
             en.printStackTrace();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
