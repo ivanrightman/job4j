3f7093edf87bd3295bd1f4db36e3ab2c9a6aa471
ivanrightman <vivasvse@gmail.com>
2020-03-26 20:19:49 +0500

    1.1. Тесты на ValidateInput. [#223313]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.tracker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateInputTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        String[] data = {"one", "1"};
+        ValidateStubInput input = new ValidateStubInput(data);
+        input.askInt("Enter");
+        assertThat(
+                new String(out.toByteArray()),
+                is(String.format("Please enter valid data again.%n"))
+        );
+    }
+
+    @Test
+    public void whenMaxInput() {
+        String[] data = {"7", "1"};
+        ValidateStubInput input = new ValidateStubInput(data);
+        input.askInt("Enter", 6);
+        assertThat(
+                new String(out.toByteArray()),
+                is(String.format("Please select key from menu.%n"))
+        );
+    }
+
+}
+
