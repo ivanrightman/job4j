4ce7e285020c3e88632e72a9915bfcb5e931fb28
Sfka <v9226132634@gmail.com>
2020-04-06 12:51:32 +0500

    3. Банковские переводы. [#10038]

--- chapter_002/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -18,7 +18,9 @@ public class BankService {
                 for (List<Account> value : this.users.values()) {
                     if (!value.contains(account)) {
                         value.add(account);
-                        }
+                    } else {
+                        System.out.println("Такой счет: " + account.getRequisite() + " уже есть у пользователя");
+                    }
                 }
             }
         }
@@ -35,19 +37,28 @@ public class BankService {
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        Account account = null;
+        Account account = new Account(requisite, -1);
         User user = findByPassport(passport);
         for (User key : this.users.keySet()) {
             if (key.equals(user)) {
                 for (List<Account> value : this.users.values()) {
-                    if (value.get(0).getRequisite().contains(requisite)) {
+                    int accIndex = value.indexOf(account);
+                    if (accIndex != -1 && value.get(accIndex).getRequisite().contains(requisite)) {
                         for (Account el : value) {
-                            account = el;
+                            if (el.getRequisite() == requisite) {
+                                account = el;
+                            }
                         }
                     } else {
                         System.out.println("Нет такого счета: " +  requisite);
+                        account = null;
                     }
                 }
+                if (account != null) {
+                    break;
+                }
+            } else {
+                account = null;
             }
         }
         return account;
@@ -56,6 +67,29 @@ public class BankService {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
+        Account srcAcc = findByRequisite(srcPassport, srcRequisite);
+        Account destAcc = findByRequisite(destPassport, destRequisite);
+        for (User key : users.keySet()) {
+            for (List<Account> value : users.values()) {
+                if (value.contains(srcAcc) && value.contains(destAcc) && srcAcc.getBalance() >= amount) {
+                    int srcAccIndex = value.indexOf(srcAcc);
+                    int destAccIndex = value.indexOf(destAcc);
+                    double newSrcBalance = srcAcc.getBalance() - amount;
+                    double newDestBalance = destAcc.getBalance() + amount;
+                    srcAcc = (new Account(srcRequisite, newSrcBalance));
+                    destAcc = (new Account (destRequisite, newDestBalance));
+                    value.set(srcAccIndex, srcAcc);
+                    value.set(destAccIndex, destAcc);
+                    rsl = true;
+                } else if (!value.contains(srcAcc)){
+                    System.out.println("Не найден счет для списания: " + srcRequisite);
+                } else if (!value.contains(destAcc)){
+                    System.out.println("Не найден счет для зачисления: " + destRequisite);
+                } else {
+                    System.out.println("На счете: " + srcRequisite + " недостаточно денег для списания");
+                }
+            }
+        }
         return rsl;
     }
 
@@ -79,7 +113,8 @@ public class BankService {
         System.out.println(bs.users.values());
         ArrayList<Account> accl = new ArrayList(bs.users.values());
         System.out.println(accl.get(0));
-        int index1 = accl.indexOf(accl);
+        int index1 = accList.indexOf(new Account("1234", -1));
         System.out.println(index1);
+        System.out.println(accl.contains(accl.get(index1)));
     }
 }
--- chapter_002/src/test/java/ru/job4j/bank/BankServiceTest.java
+++ chapter_002/src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -22,6 +22,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        //Account result = bank.findByRequisite("34", "5546");
         assertNull(bank.findByRequisite("34", "5546"));
     }
 
@@ -64,5 +65,38 @@ public class BankServiceTest {
         bank.addAccount(user.getPassport(), new Account("113", 50D));
         bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
         assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
+    }
+
+    @Test
+    public void transferMoneyWhenNotEnoughMoney() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("113", 50D));
+        boolean result = bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 300D);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void transferMoneyWhenSrcAccNotExist() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("113", 50D));
+        boolean result = bank.transferMoney(user.getPassport(), "asdf", user.getPassport(), "113", 140D);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void transferMoneyWhenDestAccNotExist() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("113", 50D));
+        boolean result = bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "asgh", 130D);
+        assertThat(result, is(false));
     }
 }
