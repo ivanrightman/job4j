72c1bc3272abed48942d1513fa21cf4ce83e3b69
Sfka <v9226132634@gmail.com>
2020-04-07 15:03:32 +0500

    3. Банковские переводы. [#10038] - испр

--- chapter_002/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -1,6 +1,5 @@
 package ru.job4j.bank;
 
-import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 import java.util.*;
 
 public class BankService {
@@ -13,16 +12,10 @@ public class BankService {
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
-        for (User key : this.users.keySet()) {
-            if (key.equals(user)) {
-                for (List<Account> value : this.users.values()) {
-                    if (!value.contains(account)) {
-                        value.add(account);
-                    } else {
-                        System.out.println("Такой счет: " + account.getRequisite() + " уже есть у пользователя");
-                    }
-                }
-            }
+        if (!this.users.get(user).contains(account)) { //получить список по user и проверить по contains
+            this.users.get(user).add(account);
+        } else {
+            System.out.println("Такой счет: " + account.getRequisite() + " уже есть у пользователя");
         }
     }
 
@@ -31,6 +24,7 @@ public class BankService {
         for (User el : users.keySet()) {
             if (el.getPassport().equals(passport)) {
                 user = el;
+                break;
             }
         }
         return user;
@@ -39,27 +33,17 @@ public class BankService {
     public Account findByRequisite(String passport, String requisite) {
         Account account = new Account(requisite, -1);
         User user = findByPassport(passport);
-        for (User key : this.users.keySet()) {
-            if (key.equals(user)) {
-                for (List<Account> value : this.users.values()) {
-                    int accIndex = value.indexOf(account);
-                    if (accIndex != -1 && value.get(accIndex).getRequisite().contains(requisite)) {
-                        for (Account el : value) {
-                            if (el.getRequisite() == requisite) {
-                                account = el;
-                            }
-                        }
-                    } else {
-                        System.out.println("Нет такого счета: " +  requisite);
-                        account = null;
-                    }
-                }
-                if (account != null) {
-                    break;
-                }
+        if (user != null) {
+            int accIndex = this.users.get(user).indexOf(account);
+            if (accIndex != -1) {
+                account = this.users.get(user).get(accIndex);
             } else {
+                System.out.println("Нет такого счета: " +  requisite);
                 account = null;
             }
+        } else {
+            System.out.println("Нет такого юзера: " +  passport);
+            account = null;
         }
         return account;
     }
@@ -69,26 +53,20 @@ public class BankService {
         boolean rsl = false;
         Account srcAcc = findByRequisite(srcPassport, srcRequisite);
         Account destAcc = findByRequisite(destPassport, destRequisite);
-        for (User key : users.keySet()) {
-            for (List<Account> value : users.values()) {
-                if (value.contains(srcAcc) && value.contains(destAcc) && srcAcc.getBalance() >= amount) {
-                    int srcAccIndex = value.indexOf(srcAcc);
-                    int destAccIndex = value.indexOf(destAcc);
-                    double newSrcBalance = srcAcc.getBalance() - amount;
-                    double newDestBalance = destAcc.getBalance() + amount;
-                    srcAcc = (new Account(srcRequisite, newSrcBalance));
-                    destAcc = (new Account (destRequisite, newDestBalance));
-                    value.set(srcAccIndex, srcAcc);
-                    value.set(destAccIndex, destAcc);
-                    rsl = true;
-                } else if (!value.contains(srcAcc)){
-                    System.out.println("Не найден счет для списания: " + srcRequisite);
-                } else if (!value.contains(destAcc)){
-                    System.out.println("Не найден счет для зачисления: " + destRequisite);
-                } else {
-                    System.out.println("На счете: " + srcRequisite + " недостаточно денег для списания");
-                }
+        if (srcAcc != null && destAcc != null) {
+            if (srcAcc.getBalance() >= amount) {
+                double newSrcBalance = srcAcc.getBalance() - amount;
+                double newDestBalance = destAcc.getBalance() + amount;
+                srcAcc.setBalance(newSrcBalance);
+                destAcc.setBalance(newDestBalance);
+                rsl = true;
+            } else {
+                System.out.println("На счете: " + srcRequisite + " недостаточно денег для списания");
             }
+        } else if (srcAcc == null){
+            System.out.println("Не найден счет для списания: " + srcRequisite);
+        } else if (destAcc == null){
+            System.out.println("Не найден счет для зачисления: " + destRequisite);
         }
         return rsl;
     }
