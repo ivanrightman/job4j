edb55688998925eec6317fd571a5c9969a6044fd
Sfka <v9226132634@gmail.com>
2020-04-13 15:04:17 +0500

    Отсортировать департаменты [#34352] - испр2

--- chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,25 +5,13 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int min = Math.min(left.length(), right.length());
-        //char[] leftCh = left.toCharArray();
-        //char[] rightCh = right.toCharArray();
-        int equation = 0;
-        int slashCount = 0;
-        for (int i = 0; i < min; i++) {
-            if (left.charAt(i) == right.charAt(i)) {
-                equation++;
-            }
-            if (left.charAt(i) == 47 && right.charAt(i) == 47) { // 47 это "/"
-                slashCount++;
-            }
-            if (left.charAt(i) != right.charAt(i)) {
-                if (equation == min - 1 && slashCount > 0) {
-                    return Character.compare(left.charAt(i), right.charAt(i));
-                }
-                return Character.compare(right.charAt(i), left.charAt(i));
-            }
+        String[] fromLeft = left.split("/");
+        String[] fromRight = right.split("/");
+        int result;
+        result = fromRight[0].compareTo(fromLeft[0]);
+        if (result == 0) {
+            result = left.compareTo(right);
         }
-        return left.length() - right.length();
+        return result;
     }
 }
